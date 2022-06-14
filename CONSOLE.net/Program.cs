bc4adf21f003fa41f56e8aeecc7cc479482fb190
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-06-14 14:58:17 -0300

    criando_uuma_aplicacao_console

---
+++ CONSOLE.net/Program.cs
@@ -0,0 +1,11 @@
+﻿NewMethod();
+
+static void NewMethod()
+{
+    int numeroDeVezes = 10;
+
+    for (int i =0 ; i < numeroDeVezes; i++)
+    {
+        Console.WriteLine($"{i} programa .net");
+    }
+}
